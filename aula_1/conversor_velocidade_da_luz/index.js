cb1bc3322614ed23b7c39d961f8485fca2429f23
Rafael Castro <81026101+rafaelcastrobr@users.noreply.github.com>
2021-03-25 17:49:34 -0300

    Create index.js

---
+++ aula_1/conversor_velocidade_da_luz/index.js
@@ -0,0 +1,9 @@
+var anosLuz = parseFloat(prompt("Quantos Anos Luz Você Quer transformar em Km?"))
+
+
+var kM = anosLuz / 0.00000000000010570
+
+var kM = kM.toFixed(1)
+
+alert("É um total de: " + kM + " Km")
+alert("Esquece, é muito longe.")
